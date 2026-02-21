26903baea2e520aea6f4b668f55fa5db56f2e241
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2026-02-21 00:06:36 -0800

    remove unwraps and use panic

--- src/app.rs
+++ src/app.rs
@@ -17,13 +17,21 @@ impl App {
         let mut exit = false;
 
         while !exit {
-            terminal.draw(|frame| page.as_ref().render(frame).unwrap())?;
-            match events_rx.recv() {
+            terminal.draw(|frame| {
+                if let Err(err) = page.as_ref().render(frame) {
+                    panic!("{err}")
+                }
+            })?;
+            let result = match events_rx.recv() {
                 Ok(Event::UserInput(key_event)) => {
                     page.as_ref()._handle_key_event(key_event, &mut exit)
                 }
-                Err(err) => panic!("Error receiving crossterm event {err}"),
+                Err(_err) => Err("Error receiving crossterm event {err}".to_string()),
             };
+
+            if let Err(err) = result {
+                panic!("{err}")
+            }
         }
 
         Ok(())
--- src/pages/mod.rs
+++ src/pages/mod.rs
@@ -7,14 +7,15 @@ pub trait Page: StatefulWidget {
     fn render(&self, frame: &mut Frame) -> Result<(), String>;
     fn handle_key_event(&self, key_event: KeyEvent) -> Result<(), String>;
 
-    fn _handle_key_event(&self, key_event: KeyEvent, exit: &mut bool) {
+    fn _handle_key_event(&self, key_event: KeyEvent, exit: &mut bool) -> Result<(), String> {
         if key_event.kind == KeyEventKind::Press
             && key_event.modifiers == KeyModifiers::CONTROL
             && key_event.code == KeyCode::Char('c')
         {
-            *exit = true
+            *exit = true;
+            Ok(())
         } else {
-            self.handle_key_event(key_event).unwrap();
+            self.handle_key_event(key_event)
         }
     }
 }
