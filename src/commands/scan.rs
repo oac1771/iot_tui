e8c2e15e47a30cf3675813bf829c830259aa38ca
Omar Carey <omarcarey@Omars-MacBook-Pro-2.local>
2026-02-21 14:19:59 -0800

    add entral scanning

--- src/commands/scan.rs
+++ src/commands/scan.rs
@@ -1,14 +1,15 @@
 use crate::state::StateClient;
 use iot_sdk::central::Central;
 
+const IOT_LOCAL_NAME: &str = "TrouBLE [Trouble Example]";
+
 pub struct ScanCmd;
 
 impl ScanCmd {
-    pub async fn handle(state_client: &StateClient) -> Result<(), String> {
-        // let _central = Central::new().await.map_err(|err| err.to_string())?;
-        state_client
-            .update_scan_items(vec![String::from("fooo"); 10])
-            .unwrap();
+    pub async fn handle(_state_client: &StateClient) -> Result<(), String> {
+        let central = Central::new().await.map_err(|err| err.to_string())?;
+        let _peripheral = central.find_peripheral(IOT_LOCAL_NAME).await.map_err(|err| err.to_string())?;
+
         Ok(())
     }
 }
